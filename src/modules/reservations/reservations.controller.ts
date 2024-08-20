c96c122f7891bc08aadfd7e458d2392dc25ba628
Lucas_Pielvitori <pielvitorilucas@gmail.com>
2024-08-20 16:08:59 -0300

    logica rutas reservations

--- src/modules/reservations/reservations.controller.ts
+++ src/modules/reservations/reservations.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 
 @Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Get()
+  findAll() {
+    return this.reservationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reservationsService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.reservationsService.remove(id);
+  }
 }
---
+++ src/modules/reservations/reservations.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ReservationsRepository {
+  constructor(
+    @InjectRepository(Reservations)
+    private reservationsRepository: Repository<Reservations>,
+  ) {}
+
+  async findAll() {
+    return this.reservationsRepository.reservations;
+  }
+
+  async findOne(id: string) {
+    const reservation = this.reservationsRepository.findOne({ where: { id } });
+    if (!reservation) {
+      throw new NotFoundException(`Reservation with ID ${id} not found`);
+    }
+    return reservation;
+  }
+
+  async remove(id: string) {
+    const reservation = await this.reservationsRepository.findOneBy({ id });
+    this.reservationsRepository.remove(reservation);
+  }
+}
