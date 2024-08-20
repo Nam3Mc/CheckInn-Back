026fdf93ec67d45c188e179f8169ceb0dcc04951
Juan Sebastian Lombo <lombofrancojuansebastian@gmail.com>
2024-08-20 12:39:13 -0300

    rutas de usuarios delete get/id get /mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './modules/rooms/rooms.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ReservationsModule } from './modules/reservations/reservations.module';
+import { AppService } from './app.service';
 
 
 @Module({
-  imports: [],
+  imports: [UsersModule,RoomsModule,AuthModule,ReservationsModule],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/modules/reservations/reservations.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+
+@Controller('reservations')
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+}
