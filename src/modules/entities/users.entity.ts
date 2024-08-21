3972d83690ecaa095261375d1145d92123102a65
ptreacey <ptreacey06@gmail.com>
2024-08-21 13:54:08 -0300

    entities y rooms seeder

--- src/modules/entities/users.entity.ts
+++ src/modules/entities/users.entity.ts
@@ -1,5 +1,17 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn, } from 'typeorm';
 import { Account } from './accounts.entity';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  Matches,
+  MaxLength,
+  Validate,
+  MinLength,
+  IsNumber,
+  IsEmpty,
+} from 'class-validator';
+import { MatchPassword } from 'src/decorators/matchPassword.decorator';
 
 export enum Roll {
   ADMIM = 'admin',
@@ -14,7 +26,7 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ default: Roll.USER })
+  @Column({ default: Roll.GUEST })
   roll: Roll;
 
   @Column({ length: 50, nullable: false })
@@ -26,9 +38,23 @@ export class User {
   @Column({ length: 50, nullable: false })
   phone: number;
 
-  @Column({ length: 50, nullable: false })
+  @IsString()
+  @Matches(
+    /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,15}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  @MinLength(8)
+  @MaxLength(15)
   password: string;
 
-  @OneToMany(() => Account, (account) => account.user_)
-  account: Account[];
+
+  @IsNotEmpty()
+  @Validate(MatchPassword, ['password'])
+  passwordConfirmation: string;
+
+  @OneToMany(() => Account, (account) => account.user)
+  accounts: Account[];
 }
