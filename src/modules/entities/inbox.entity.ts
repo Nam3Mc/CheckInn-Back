ce0a7e74c1fef1585fbb13aab056d087d5ec6275
Juan Sebastian Lombo <lombofrancojuansebastian@gmail.com>
2024-08-23 15:04:51 -0300

    agregado el accontId al usuario en signup

--- src/modules/entities/inbox.entity.ts
+++ src/modules/entities/inbox.entity.ts
@@ -13,14 +13,12 @@ export class Inbox {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  // Relación uno a uno con la entidad Account.
-  // Un Inbox pertenece a un solo Account y un Account tiene un solo Inbox.
+
   @OneToOne(() => Account, (account) => account.inbox)
   @JoinColumn()
   account: Account;
 
-  // Relación muchos a uno con la entidad Comment.
-  // Muchos Inbox pueden estar asociados con un único Comment.
+
   @ManyToOne(() => Comment, (comment) => comment.inbox)
   comment: Comment[];
 }
