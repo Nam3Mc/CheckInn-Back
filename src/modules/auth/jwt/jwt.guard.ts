be03d2c5eed911d1bb1e13fd8db780bfc3ac7172
Juan Sebastian Lombo <lombofrancojuansebastian@gmail.com>
2024-08-28 09:48:14 -0300

    implentacion de Auth0 - hacer npm i

---
+++ src/modules/auth/jwt/jwt.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      return false;
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+      this.jwtService.verify(token);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
