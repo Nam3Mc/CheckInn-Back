c521965c1b65a0c464d37082d23463161e00876c
Lucas_Pielvitori <pielvitorilucas@gmail.com>
2024-09-08 00:01:19 -0300

    mejora manejo pagos

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,7 +21,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
+  app.use(bodyParser.json());
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
